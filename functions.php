4dca08955da46430e004ddfa1c0bca54ae029ec7
happywebdiva <git@happywebdiva.com>
2015-09-28 17:34:43 -0500

    Cleaning up sidebar widgets

--- functions.php
+++ functions.php
@@ -4,27 +4,6 @@
 add_action( 'widgets_init', 'aapcse_widgets_init' );
 function aapcse_widgets_init() {
 	register_sidebar( array(
-		'name' => 'About Sidebar',
-		'id' => 'about-widgets',
-		'description' => __( 'Drag to create or update the sidebar for pages using the "About Us Template"'),
-		'before_widget' => '<li class="custom-widget">',
-		'after_widget' => "</div>",
-	) );
-	register_sidebar( array(
-		'name' => 'Education Sidebar',
-		'id' => 'educ-widgets',
-		'description' => __( 'Drag to create or update the sidebar for pages using the Education Template'),
-		'before_widget' => '<li class="custom-widget">',
-		'after_widget' => "</div>",
-	) );
-	register_sidebar( array(
-		'name' => 'Ministry Sidebar',
-		'id' => 'ministry-widgets',
-		'description' => __( 'Drag to create or update the sidebar for pages using the Ministry Template'),
-		'before_widget' => '<li class="custom-widget">',
-		'after_widget' => "</div>",
-	) );
-	register_sidebar( array(
 		'name' => 'Footer Widget',
 		'id' => 'footer-widget',
 		'description' => __( 'The footer widget area. Add a text widget to include your address and phone.'),
