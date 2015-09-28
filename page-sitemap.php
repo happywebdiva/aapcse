80cd924f0223994b3a668f74cef50ebbd853c6fc
happywebdiva <git@happywebdiva.com>
2015-09-28 17:34:11 -0500

    Improving look of Site Map template

--- page-sitemap.php
+++ page-sitemap.php
@@ -8,33 +8,26 @@
 	<?php 
     while ( have_posts() ) : the_post();
 	?>
-	<div class="eight columns alpha"><header class="entry-header"><h1 id="page-title"><?php the_title(); ?></h1></header></div>
-  <div class="eight columns omega"><div class="hentry" id="searchbox-sitemap"><?php get_search_form(); ?></div></div>
+	<header class="entry-header"><h1 id="page-title"><?php the_title(); ?></h1></header>
   <div class="eight columns alpha " role="main">
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <div class="entry">
-    <?php
-      the_content();
-		?>
-  </div><!--.entry-->
-</article>
 	<?php
     endwhile;
   ?>
-    <div class="hentry">
-      <h2>Blog Archives</h2>
-      <ul>
-        <?php wp_get_archives('type=monthly&limit=12'); ?>
-      </ul>
-		</div>      
-  </div><!--.eight-1.columns-->
-	<div class="eight columns omega">
     <div class="hentry">
       <h2>List of Pages</h2>
       <ul>
         <?php wp_list_pages('title_li=&depth=2&sort_column=post_title'); ?>
       </ul>
-		</div>      
+    </div>      
+  </div><!--.eight-1.columns-->
+	<div class="eight columns omega">
+    <div class="hentry">
+      <h2>Blog Archives</h2>
+      <ul>
+        <?php wp_get_archives('type=monthly&limit=12'); ?>
+      </ul>
+    </div>      
+    <div class="hentry" id="searchbox-sitemap" style="margin-top: 49px;"><?php get_search_form(); ?></div>
   </div><!--.eight-2.columns-->
 </div><!--.sixteen.columns-->
 <?php get_footer(); ?>
